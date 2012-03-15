e3876ab16488943317f6f2684f513ceac492448e
Stephen A. Wood <saw@jlab.org>
2012-03-15 17:11:23 -0400

    Start a raw hit list class

---
+++ src/THcRawHitList.cxx
@@ -0,0 +1,17 @@
+//*-- Author : Stephen Wood  
+
+//////////////////////////////////////////////////////////////////////////
+//
+// THcRawHistList
+//
+// Class to build raw hit lists from data
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "THcRawHitList.h"
+
+using namespace std;
+
+THcRawHitList::THcRawHitList(const char* classname, Int_t size=1000, Int_t detctorid) {
+  //  Create it 
+  
