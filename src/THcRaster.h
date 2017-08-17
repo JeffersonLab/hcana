1dce3be103ad76f9e009a87109b9c7bcbec4cf44
bduran <bduran@jlab.org>
2017-08-17 14:40:50 -0400

    added new variables for the 2nd raster, edited the code to be able to read both pairs of rasters

--- src/THcRaster.h
+++ src/THcRaster.h
@@ -1,3 +1,6 @@
+
+//author Burcu Duran - Melanie Rehfuss (2017) 
+
 #ifndef ROOT_THcRaster
 #define ROOT_THcRaster
 
@@ -15,6 +18,7 @@
 #include "THcRasterRawHit.h"
 #include "THaCutList.h"
 
+
 class THcRaster : public THaBeamDet, public THcHitList {
 
  public:
@@ -22,9 +26,20 @@ class THcRaster : public THaBeamDet, public THcHitList {
   THcRaster(const char* name, const char* description ="",THaApparatus* a = NULL );
 
   ~THcRaster();
+
+  void  Clear(Option_t* opt="");
+  void    AccumulatePedestals(TClonesArray* rawhits);
+  void    CalculatePedestals();
+ 
+  Int_t  Decode( const THaEvData& );
+  Int_t  ReadDatabase( const TDatime& date );
+  Int_t DefineVariables( EMode mode );
+
   EStatus    Init( const TDatime& run_time );
 
-  Int_t      Decode( const THaEvData& );
+  void  InitArrays() { /* do nothing */;}
+  void  DeleteArrays() { /* do nothing */;}
+ 
   Int_t      Process();
 
   //  TVector3 GetPosition()  const { return fPosition[2]; }
@@ -34,12 +49,16 @@ class THcRaster : public THaBeamDet, public THcHitList {
   Double_t GetCurrentX() { return fRawPos[0]; }
   Double_t GetCurrentY() { return fRawPos[1]; }
 
+  THcRaster();
+
  protected:
 
-  /* void InitializeReconstruction(); */
-  Int_t  ReadDatabase( const TDatime& date );
-  Int_t DefineVariables( EMode mode );
+  //event information
 
+  Int_t fNhits;
+
+  /* void InitializeReconstruction(); */
+  
   Double_t       fgpbeam;   //beam momentum
   Double_t       fgfrx_dist;     //Distance of raster to target
   Double_t       fgfry_dist;
@@ -49,31 +68,42 @@ class THcRaster : public THaBeamDet, public THcHitList {
   Double_t       fgbeam_ypoff; // Beam offsets
   Int_t          fgusefr;   /* Use Raster for beam position */
 
-  Double_t       fRawXADC;  // X raw ADC
-  Double_t       fRawYADC;  // Y raw ADC
-  Double_t       fXADC;     // X ADC
-  Double_t       fYADC;     // Y ADC
-  Double_t       fXpos;     // X position
-  Double_t       fYpos;     // Y position
+  Double_t       FRXA_rawadc;  // XA raw ADC
+  Double_t       FRYA_rawadc;  // YA raw ADC
+  Double_t       FRXB_rawadc;  // XB raw ADC
+  Double_t       FRYB_rawadc;  // YB raw ADC
+  Double_t       fXA_ADC;     // XA ADC
+  Double_t       fYA_ADC;     // YA ADC
+  Double_t       fXB_ADC;     // XB ADC
+  Double_t       fYB_ADC;     // YB ADC
+  Double_t       fXA_pos;     // XA position
+  Double_t       fYA_pos;     // YA position
+  Double_t       fXB_pos;     // XB position
+  Double_t       fYB_pos;     // YB position
+
+  Double_t       fFrXA_ADC_zero_offset;
+  Double_t       fFrYA_ADC_zero_offset;
+  Double_t       fFrXB_ADC_zero_offset;
+  Double_t       fFrYB_ADC_zero_offset;
 
 
-  Double_t       fPedADC[2];     // ADC poedestals
-  Double_t       fAvgPedADC[2];     // Avergage ADC poedestals
+  Double_t       fPedADC[4];     // ADC poedestals
+  //Double_t       fAvgPedADC[4];     // Avergage ADC poedestals
 
   Double_t       fRawPos[2];     // current in Raster ADCs for position
-  TVector3       fPosition[2];   // Beam position at 1st, 2nd BPM or at the target (meters)
+  TVector3       fPosition[4];   // Beam position at 1st, 2nd BPM or at the target (meters)
   TVector3       fDirection;
+
+  TClonesArray* frPosAdcPulseIntRaw;
 
  private:
   Bool_t    fAnalyzePedestals;
   Int_t     fNPedestalEvents;
   Double_t  fFrCalMom;
-  Double_t  fFrXADCperCM;
-  Double_t  fFrYADCperCM;
-
-
-  void   CalculatePedestals();
-  void   AccumulatePedestals(TClonesArray* rawhits);
+  Double_t  fFrXA_ADCperCM;
+  Double_t  fFrYA_ADCperCM;
+  Double_t  fFrXB_ADCperCM;
+  Double_t  fFrYB_ADCperCM;
 
   ClassDef(THcRaster, 0);	// add THcRaster to ROOT library
 };
