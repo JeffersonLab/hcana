a5955b77ddc20bfddc0985e4508539c9d0a32679
Stephen A. Wood <saw@jlab.org>
2018-03-15 10:46:23 -0400

    Simplify THcFormula so that code from THaFormula is not duplicated.    THcFormula::DefinedValue is removed as      THaFormula::DefinedValue now handles the kCutScaler and kCutNCalled types

--- src/THcFormula.h
+++ src/THcFormula.h
@@ -23,14 +23,11 @@ public:
   THcFormula& operator=( const THcFormula& rhs );
   virtual ~THcFormula();
 
-  virtual Double_t DefinedValue( Int_t i);
   virtual Int_t    DefinedCut( TString& variable);
   virtual Int_t    DefinedGlobalVariable( TString& variable);
 
 protected:
 
-  enum {kCutScaler = kVarFormula+1};
-  enum {kCutNCalled = kCutScaler+1};
   const THcParmList* fParmList; // Pointer to list of parameters
   ClassDef(THcFormula,0) // Formula with cut scalers
 };
