9712a5efe09dd89a10f55e2bb22de818eb5be8ba
Carlos Yero <cyero002@fiu.edu>
2018-04-25 18:20:27 -0400

    added a check to see if there was a golden track in both spectrometers

--- src/THcCoinTime.cxx
+++ src/THcCoinTime.cxx
@@ -152,20 +152,25 @@ Int_t THcCoinTime::Process( const THaEvData& evdata )
   pionMass = 139.570/1000.0;    //charged pion mass in GeV/c^2
 
   SHMScentralPathLen =  18.1*100;    //cm       //TODO: put in a GEN Param file, and read it from there 
-  HMScentralPathLen =  22.*100;   //cm
+  HMScentralPathLen =  22.*100;   //cm          //TODO: put in a GEN Param file, and read it from there 
  
+  //Check if there was a golden track in both arms
+  if (!theHadTrack && !theElecTrack) return 1;
+
   //electron arm
-  theElecTrack->GetP();        //hadron golden track arm momentum
-  theElecTrack->GetTTheta();    //xp_fp
-  theElecTrack->GetDp();        //electron arm deltaP 
-  theElecTrack->GetFPTime();    //electron arm focal plane time
+  elec_P = theElecTrack->GetP();              //electron golden track arm momentum
+  elec_th = theElecTrack->GetTTheta();        //xp_tar
+  elec_dP = theElecTrack->GetDp();            //electron arm deltaP 
+  elec_FPtime = theElecTrack->GetFPTime();    //electron arm focal plane time
   
   //hadron arm
-  theHadTrack->GetP();    //hadron golden track arm momentum
-  theHadTrack->GetRX();  //x_fp
-  theHadTrack->GetRTheta();  //xp_fp
-  theHadTrack->GetRPhi();  //yp_fp
-  theHadTrack->GetFPTime();  //hadron-arm focal plane time
+  had_P = theHadTrack->GetP();              //hadron golden track arm momentum
+  had_xfp = theHadTrack->GetRX();           //x_fp
+  had_xpfp = theHadTrack->GetRTheta();      //xp_fp
+  had_ypfp = theHadTrack->GetRPhi();        //yp_fp
+  had_FPtime = theHadTrack->GetFPTime();    //hadron-arm focal plane time
+
+  
 
 
   return 0;
