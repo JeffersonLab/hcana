020413fbc5f8e4e26a854b454e7b412a8292302e
hallc-online <hallconline@gmail.com>
2018-08-15 13:08:54 -0400

    Modify THcExtTarCor to calculate xsieve, ysieve for spectrometer
    1) THcExtTarCor.h
       a) add variables fxsieve and fysieve
       b) Added clear method so that fxsieve and fysieve could be set to kBig at start of each event.
    
    2) THcExtTarCor.cxx
       a) Add variables extcor.xsieve and extcor.ysieve to tree
       b) Added clear method.
       c) Added calculation of xsieve and ysieve for golden track. The
          calculation depends on spectrometer. SHMS includes delta dependence
          when calculating ysieve.

--- src/THcExtTarCor.h
+++ src/THcExtTarCor.h
@@ -20,10 +20,13 @@ public:
   
   virtual Int_t     Process( const THaEvData& );
 
+  Double_t fxsieve,fysieve;
+
 protected:
 
   virtual Int_t DefineVariables( EMode mode = kDefine );
   virtual Int_t ReadDatabase( const TDatime& date );
+   virtual void      Clear( Option_t* opt="" );
 
   ClassDef(THcExtTarCor,0)   //Extended target corrections module
 };
