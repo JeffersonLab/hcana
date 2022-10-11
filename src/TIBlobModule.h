0cc8738dd0651bed6d64f1d1445153faf609af72
Ole Hansen <ole@jlab.org>
2022-10-10 21:33:51 -0400

    TIBlobModule: Use custom decoder for this module's data format
    Closes Redmine issue 676 (https://redmine.jlab.org/issues/676).

--- src/TIBlobModule.h
+++ src/TIBlobModule.h
@@ -14,6 +14,7 @@
 /////////////////////////////////////////////////////////////////////
 
 #include "PipeliningModule.h"
+#include <string>
 
 namespace Decoder {
 
@@ -34,12 +35,18 @@ public:
    virtual Int_t Decode(const UInt_t*) { return 0; }
    virtual UInt_t LoadSlot( THaSlotData *sldat, const UInt_t *evbuffer, const UInt_t *pstop );
    virtual UInt_t LoadSlot( THaSlotData *sldat, const UInt_t* evbuffer, UInt_t pos, UInt_t len);
+   virtual UInt_t LoadBank( THaSlotData* sldat, const UInt_t* evbuffer, UInt_t pos, UInt_t len );
 
  protected:
+  virtual UInt_t LoadNextEvBuffer( THaSlotData *sldat );
 
  private:
 
-   static const size_t NTICHAN = 3;
+   static const size_t NTICHAN = 4;  // maximum number of data words per event
+   UInt_t fNfill;        // Number of filler words at end of current bank
+   Bool_t fHasTimestamp; // Event data contain timestamp (default)
+
+   std::string Here( const char* function );
 
    static TypeIter_t fgThisType;
    ClassDef(TIBlobModule,0)  //  TIBlob of a module;
