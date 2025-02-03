09bdcc5ab79c7caea7fba6e69b297a0c88281dee
sanghwapark <sanghwapark@gmail.com>
2025-02-03 14:24:05 -0500

    Add config output tree for event 137

--- src/THcEvt137Handler.cxx
+++ src/THcEvt137Handler.cxx
@@ -6,6 +6,7 @@
 #include "THaEvData.h"
 #include "DAQconfig.h"
 #include "THcParmList.h"
+#include "TROOT.h"
 #include <iostream>
 #include <sstream>
 
@@ -20,16 +21,24 @@ using namespace std;
 //_______________________________________________________________
 THcEvt137Handler::THcEvt137Handler( const char* name,
 				    const char* description ) :
-  THaEvtTypeHandler(name, description), fNDecoded(0)
+  THaEvtTypeHandler(name, description), fNDecoded(0),
+  fConfigTree(nullptr), fMakeConfigTree(true), fMakeParms(true),
+  fCounter(0)
 {
 }
 
 //_______________________________________________________________
 THcEvt137Handler::~THcEvt137Handler()
 {
-  for( auto& cfg : fConfigParmList ) {
-    gHcParms->RemoveString(cfg.par);
+  if(fMakeParms) {
+    for( auto& cfg : fConfigDataMap )
+      gHcParms->RemoveName(Form("g_%s", cfg.first.data()));
   }
+
+  if( !TROOT::Initialized()) {
+    delete fConfigTree;
+  }
+  
 }
 
 //_______________________________________________________________
@@ -38,10 +47,21 @@ THaAnalysisObject::EStatus THcEvt137Handler::Init( const TDatime& date )
 
   // default event type 
   if( fEvtTypes.empty() ) {
-    fEvtTypes.push_back(137);
+    fEvtTypes.emplace_back(137);
   }
 
   return THaEvtTypeHandler::Init(date);
+}
+
+//_______________________________________________________________
+Int_t THcEvt137Handler::End( THaRunBase* )
+{
+  cout << "THcEvt137Handler::End" << endl;
+
+  // Save the tree into the output 
+  if(fMakeConfigTree) { SaveConfigData(); }
+
+  return 0;
 }
 
 //_______________________________________________________________
@@ -52,101 +72,144 @@ void THcEvt137Handler::AddEvtType( UInt_t evtype )
   // Instead, we set the event types only relevant for this class
   
   if( std::find(fEvtTypes.begin(), fEvtTypes.end(), evtype ) == fEvtTypes.end() )
-    fEvtTypes.push_back(evtype);
+    fEvtTypes.emplace_back(evtype);
 }
 
-//_______________________________________________________________
-void THcEvt137Handler::AddParameter(std::string parname, std::string keyname)
-{
-  fConfigParmList.push_back( {Form("g_%s", parname.data()), keyname} );
-}
- 
 //_______________________________________________________________
 void THcEvt137Handler::MakeParms()
 {
-  // all data parsed as a single string varaible 
+  for(auto &cfg : fConfigDataMap ) {
+    string keyname = cfg.first;
+    const auto &vals = cfg.second.pars;
 
-  for( auto& cfg : fConfigParmList ) {
-    if( !GetInfo(cfg.key.data()).empty() ) {
-      gHcParms->RemoveString(cfg.par);
-      gHcParms->AddString(cfg.par, GetInfo(cfg.key.data()) );
+    int nval = vals.size();
+
+    // Define global variables
+    gHcParms->RemoveName(Form("g_%s", keyname.data()));
+    if(nval == 1)
+      gHcParms->Define(Form("g_%s", keyname.data()), keyname.data(), vals[0]);
+    else
+      gHcParms->Define(Form("g_%s", keyname.data()), keyname.data(), vals); // vector type is supported
+  }
+
+  /*
+  const auto* pvar = gHcParms->Find("g_FADC250_ALLCH_PED_2_9");
+  if(pvar){
+    if(pvar->IsVector()){
+      auto v1 = pvar->GetValues();
+      cout << pvar->GetLen() << endl;
+      cout << v1[0] << endl;
+      cout << v1[1] << endl;
+      cout << v1[15] << endl;
     }
-  }      
+  }
+  */
 }
 
 //_______________________________________________________________
 std::string THcEvt137Handler::GetInfo(const char* keyname )
 {
-  auto it = fConfigData.find(keyname);
-  if( it != fConfigData.end() )
-    return it->second;
+  // return parameters as a single string 
+  auto it = fConfigDataMap.find(keyname);
+  if( it != fConfigDataMap.end() )
+    return it->second.pars_str;
   else
     return "";
 }
 
 //_______________________________________________________________
-UInt_t THcEvt137Handler::GetNSA( UInt_t crate, UInt_t slot )
+int THcEvt137Handler::GetNSA( UInt_t crate, UInt_t slot )
 {
-  int nsa = 0;
+  int val = -1;
   string keyname = "FADC250_NSA_" + std::to_string(crate) + "_" + std::to_string(slot);
-  string val = GetInfo(keyname.data());
-  if( val.length() > 0 )
-    nsa = std::stoi(val);
 
-  return nsa;
+  auto it = fConfigDataMap.find(keyname);
+  if( it != fConfigDataMap.end() ) {
+    val = static_cast<int>(it->second.pars[0])/4;
+  }
+  return val;
 }
 
 //_______________________________________________________________
-UInt_t THcEvt137Handler::GetNSB( UInt_t crate, UInt_t slot )
+int THcEvt137Handler::GetNSB( UInt_t crate, UInt_t slot )
 {
-  int nsb = 0;
+  int val = -1;
   string keyname = "FADC250_NSB_" + std::to_string(crate) + "_" + std::to_string(slot);
-  string val = GetInfo(keyname.data());
-  if( val.length() > 0 )
-    nsb = std::stoi(val);
- 
-  return nsb;
+
+  auto it = fConfigDataMap.find(keyname);
+  if( it != fConfigDataMap.end() ) {
+    val = static_cast<int>(it->second.pars[0])/4;
+  }
+  return val;
 }
 
 
 //_______________________________________________________________
-UInt_t THcEvt137Handler::GetNPED( UInt_t crate, UInt_t slot )
+int THcEvt137Handler::GetNPED( UInt_t crate, UInt_t slot )
 {
-  int nped = 0;
+  int val = -1;
   string keyname = "FADC250_NPED_" + std::to_string(crate) + "_" + std::to_string(slot);
-  string val = GetInfo(keyname.data());
-  if( val.length() > 0 )
-    nped = std::stoi(val);
- 
-  return nped;
+
+  auto it = fConfigDataMap.find(keyname);
+  if( it != fConfigDataMap.end() ) {
+    val = static_cast<int>(it->second.pars[0]);
+  }
+  return val;
 }
 
 //_______________________________________________________________
 Double_t THcEvt137Handler::GetPED( UInt_t crate, UInt_t slot, UInt_t ch )
 {
-  vector<Double_t> v_ped;
-
+  Double_t ped = 0;
   string keyname = "FADC250_ALLCH_PED_" + std::to_string(crate) + "_" + std::to_string(slot);
-  string val = GetInfo(keyname.data());
+  auto it = fConfigDataMap.find(keyname);
+  if( it != fConfigDataMap.end() ) {
+    ped = it->second.pars[ch];
+  }
+  return ped;
+}
 
-  if( val.length() > 0 ) {
-    istringstream ifstr(val);
-    Double_t ped;
-    while(ifstr >> ped) {
-      v_ped.push_back(ped);      
-    }      
+//_______________________________________________________________
+void THcEvt137Handler::SaveConfigData()
+{
+  // Init output tree
+  if( fMakeConfigTree && !fConfigTree ) {
+      fConfigTree = new TTree("THC", "Config parameter tree");
+  }    
+
+  vector<Double_t> fVars;
+  vector<vector<Double_t>> fArrays;
+
+  fVars.resize(fCounter);
+  fArrays.resize(fCounter);
+  UInt_t ivar = 0, iarr = 0; // counter
+
+  // Init tree
+  for( auto &cfg : fConfigDataMap ){
+    string bname = cfg.first; // branch name
+    auto bpars = cfg.second.pars; // 
+
+    int npar = bpars.size();
+
+    if(npar == 1) {
+      fConfigTree->Branch(Form("%s", bname.data()), &fVars[ivar]);
+      fVars[ivar] = bpars[0];
+      ivar++;
+    }
+    else {
+      fConfigTree->Branch(Form("%s", bname.data()), &fArrays[iarr]);
+      fArrays[iarr] = bpars;
+      iarr++;
+    }
   }
 
-  if( ch+1 > v_ped.size() )
-    return 0;
-  else
-    return v_ped[ch];
+  fConfigTree->Fill();
+  fConfigTree->Write();
 }
 
 //_______________________________________________________________
 Int_t THcEvt137Handler::Analyze( THaEvData* evdata )
 {
-
   UInt_t evtype = evdata->GetEvType();
 
   // Check event type
@@ -163,6 +226,7 @@ Int_t THcEvt137Handler::Analyze( THaEvData* evdata )
   }
 
   UInt_t roc = bankinfo.tag_;
+  fRoc.emplace_back(roc);
 
   auto* ifo = DAQInfoExtra::GetFrom(evdata->GetExtra());
   if( !ifo ) return -1;
@@ -184,28 +248,50 @@ Int_t THcEvt137Handler::Analyze( THaEvData* evdata )
       string val;
       val.reserve(line.size());
 
+      std::vector<Double_t> v_val;
+
       for( size_t j = 1, e = items.size(); j < e; ++j ) {
 	val.append(items[j]);
 	if( j + 1 != e )
 	  val.append(" ");
-      }
 
+	//for simplicity make them all double vars
+	v_val.emplace_back(std::stod(items[j]));
+      }
       // Per slot config for FADC250
+      // This assumes that SLOT information is the first line for FADC250 config list
       if( key == "FADC250_SLOT" ){
 	slot.replace(0, slot.length(), val);
       }	  
 
       // Define new key for Parm list
-      string new_key = key + "_" + std::to_string(roc) + "_" + slot;
+      string new_key = key + "_" + std::to_string(roc);
+      if( slot.length() > 0)
+	new_key = new_key + "_" + slot;
 
-      // Add Parameters
-      fConfigParmList.push_back( {Form("g_%s", new_key.data()), new_key} );
-      fConfigData.emplace( std::move(new_key), std::move(val) );
+      // Remove the existing element if the key already exists
+      // and will override with new parameters
+      if( fConfigDataMap.find(new_key) != fConfigDataMap.end() )
+	fConfigDataMap.erase(new_key);
+
+      // Add to the container
+      ConfigData config_data;
+      config_data.roc = roc;
+      config_data.slot = -1;
+      if( slot.length() > 0 )
+	config_data.slot = std::stoi(slot);
+      config_data.parname = key;
+      config_data.pars = v_val;
+      config_data.pars_str = val;
+      fConfigDataMap.emplace(std::move(new_key), std::move(config_data));
+
+      fCounter++;
     }
   }// getline
 
-  // Add to the gHcParm list
-  MakeParms();
+  // Add to the Parm List
+  if(fMakeParms)
+    MakeParms();
 
   fNDecoded++;
 
--- src/THcEvt137Handler.h
+++ src/THcEvt137Handler.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include "TTree.h"
 
 class THcEvt137Handler : public THaEvtTypeHandler {
  public:
@@ -14,34 +15,46 @@ class THcEvt137Handler : public THaEvtTypeHandler {
   
   virtual Int_t   Analyze( THaEvData *evdata );
   virtual EStatus Init( const TDatime& date );
+  virtual Int_t   End( THaRunBase* r=nullptr );
   virtual void    AddEvtType( UInt_t evtype );
-  virtual void    AddParameter(std::string parname, std::string keyname);
 
   std::vector<UInt_t> GetEvtTypes() { return fEvtTypes; }
   std::string GetInfo( const char* keyname );
 
+  void MakeConfigTree(bool make_tree) { fMakeConfigTree = make_tree; } // default is true
+  void MakeParms(bool make_parms ) { fMakeParms = make_parms; } // default is true
+
   // Some useful getter functions for FADC250
-  UInt_t GetNSA( UInt_t crate, UInt_t slot );
-  UInt_t GetNSB( UInt_t crate, UInt_t slot );
-  UInt_t GetNPED( UInt_t crate, UInt_t slot );
+  int GetNSA( UInt_t crate, UInt_t slot );
+  int GetNSB( UInt_t crate, UInt_t slot );
+  int GetNPED( UInt_t crate, UInt_t slot );
   Double_t GetPED( UInt_t crate, UInt_t slot, UInt_t ch );
 
  private:
 
   Int_t fNDecoded; // Counter for decoded type 137 event
 
-  std::vector<UInt_t> fEvtTypes;
+  TTree* fConfigTree; // Output tree for config information
+  bool fMakeConfigTree; // default is true
+  bool fMakeParms;
+  UInt_t fCounter; // rough counter of the number of parameters to save
 
-  // General parm and key pairs
-  struct ConfigParms {
-      std::string par;
-      std::string key;
+  struct ConfigData {
+    int roc;
+    int slot;
+    std::string parname;
+    std::string pars_str; // data in a single string format
+    std::vector<Double_t> pars;
   };
 
-  std::vector<ConfigParms> fConfigParmList;
-  std::map<std::string, std::string> fConfigData;
+  // with a uinque keyname
+  std::map<std::string, ConfigData> fConfigDataMap;
 
-  virtual void MakeParms();
+  std::vector<UInt_t> fRoc; // List of rocs with config info
+  std::vector<UInt_t> fEvtTypes;
+
+  void SaveConfigData();
+  void MakeParms();
 
   ClassDef(THcEvt137Handler,0)
 };
